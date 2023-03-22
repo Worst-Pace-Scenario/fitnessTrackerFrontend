61f2f6fd16958f240a13037b864055a25e9dfe20
cforlini24 <cforlini24@gmail.com>
2023-03-22 15:44:40 -0400

    fixed file order

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,2 @@
 export { default as UserRegistration } from "./UserRegistration"
 export { default as UserLogin } from "./UserLogin"
-export {default as Nav} from "./Nav";
-export {default as Home} from "./Home";
-export {default as Activities} from "./activities";
