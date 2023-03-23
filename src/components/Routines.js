d33105df2cc063f9b7f6dfca5470a762a757a07a
Charlie Jenkins <charliedj13@gmail.com>
2023-03-23 14:18:50 -0500

    thu

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -2,22 +2,7 @@ import { useState } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 
 const Routines = (props) => {
-    // const [posts, setPosts] = useState([]);
-
-    //This function is fetching all routines
-    // async function fetchRoutines() {
-    //     try {
-    //         const response = await fetch('https://fitnesstrac-kr.herokuapp.com/routines', {
-    //             headers: {
-    //                 "Content-Type": "application/json"
-    //             }
-    //         })
-
-
-    //     } catch(error) {
-    //         console.log(error)
-    //     }
-    // }
+    
 
     return (
         <div>
