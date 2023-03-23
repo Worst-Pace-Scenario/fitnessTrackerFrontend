50434a195259ba68fdaa10901c27681010a62efc
Charlie Jenkins <charliedj13@gmail.com>
2023-03-22 20:24:33 -0500

    wed

--- src/components/MyRoutines.js
+++ src/components/MyRoutines.js
@@ -1,11 +1,17 @@
 import { useState } from "react"
 import { useParams, useNavigate } from "react-router-dom"
+import "./myroutines.css"
 
 const MyRoutines = (props) => {
     const [newPostForm, setNewPostForm] = useState(false);
+    const [updateForm, setUpdateForm] = useState(false)
     const [Name, setName] = useState("");
     const [Goal, setGoal] = useState("");
     const [isPublic, setIsPublic] = useState(false)
+    const [newName, setNewName] = useState("");
+    const [newGoal, setNewGoal] = useState("")
+
+    const { id } = useParams()
 
     const navigate = useNavigate()
 
@@ -14,6 +20,25 @@ const MyRoutines = (props) => {
     //This function is toggling the newPostRequest form...
     function toggleNewForm() {
         setNewPostForm(!newPostForm)
+    }
+    function toggleNewFormUpdate() {
+        setUpdateForm(!updateForm)
+    }
+
+    //This function is fetching all the routines
+    async function fetchRoutines() {
+        try {
+            const response = await fetch(`https://fitnesstrac-kr.herokuapp.com/routines`, {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            });
+            const result = await response.json()
+
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 
 
@@ -45,7 +70,7 @@ const MyRoutines = (props) => {
     async function deleteSpecificPost(event) {
         event.preventDefault();
         try {
-            const response = await fetch(`https://fitnesstrac-kr.herokuapp.com/routines`, {
+            const response = await fetch(`https://fitnesstrac-kr.herokuapp.com/routines/${id}`, {
                 method: "DELETE",
                 headers: {
                     "Content-Type": "application/json",
@@ -63,17 +88,38 @@ const MyRoutines = (props) => {
         }
     }
 
+    //This function is updating the post via the api that was given to  us...
+    async function updateSpecificPost() {
+        try {
+            const response = await fetch (`https://fitnesstrac-kr.herokuapp.com/routines/${id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`
+                },
+                body: JSON.stringify({
+                    name: Name,
+                    goal: Goal
+                })
+            });
+            const result = await response.json()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
 
 
     return (
         <div>
 
             <div>
-                <button onClick={toggleNewForm}>Create Goal</button>
+                <button id="button" onClick={toggleNewForm}>Create Routine</button>
             </div>
 
             {
             newPostForm ? (
+                <div>
             <form onSubmit={newPostRequest}>
                 <input
                 type="text"
@@ -98,14 +144,56 @@ const MyRoutines = (props) => {
                 />
                 <label
                 htmlFor="checkbox"
-                > Make Goal Public? 
+                > Make Routine Public? 
                 </label>
             </form>
+            <button onClick={newPostRequest}>Submit</button>
+            </div>
                 ) : ""
             }
             <button 
+            id="button"
             onClick={deleteSpecificPost}
             >Delete Post</button>
+
+            <button onClick={toggleNewFormUpdate}>Update Routine</button>
+
+            
+                
+            {
+                updateForm ? (
+                    <div>
+                    <form onSubmit={updateSpecificPost}>
+                    <input
+                    type="text"
+                    placeholder="Name of Goal"
+                    value={newName}
+                    onChange={(event) => setNewName(event.target.value)}
+                    />
+                    <textarea
+                    type="text"
+                    rows="3"
+                    cols="100"
+                    placeholder="Goal Description"
+                    value={newGoal}
+                    onChange={(event) => setNewGoal(event.target.value)}
+                    />
+                </form>
+                <button onClick={updateSpecificPost}>Update Post</button>
+                </div>
+                ) : ""
+            }
+
+            {/* <button>Add Activity To Routine</button>
+            <form>
+                <option value={props.activity.name}>{props.activity.name}</option>
+                <option value={props.activity.name}>{props.activity.name}</option>
+                <option value={props.activity.name}>{props.activity.name}</option>
+                <option value={props.activity.name}>{props.activity.name}</option>
+            </form> */}
+            
+                
+
 
 
         </div>
---
+++ src/components/Routines.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+
+const Routines = (props) => {
+    // const [posts, setPosts] = useState([]);
+
+    //This function is fetching all routines
+    // async function fetchRoutines() {
+    //     try {
+    //         const response = await fetch('https://fitnesstrac-kr.herokuapp.com/routines', {
+    //             headers: {
+    //                 "Content-Type": "application/json"
+    //             }
+    //         })
+
+
+    //     } catch(error) {
+    //         console.log(error)
+    //     }
+    // }
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Routines
+
+
