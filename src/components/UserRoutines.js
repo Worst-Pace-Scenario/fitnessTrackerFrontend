b62500a9d7744b56e7748e2bb26aa0a854e0b0cb
Aamna Syed <aamna.s.syed@gmail.com>
2023-03-23 18:16:19 -0500

    userroutines was updated

--- src/components/UserRoutines.js
+++ src/components/UserRoutines.js
@@ -1,27 +1,15 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const BASE_URL ='http://fitnesstrac-kr.herokuapp.com/api/'
 
 const UserRoutines = (props) => {
 
     const { currentUser } = props 
-
+    // routines as props 
     const [ myId, setMyId ] = useState('')
     const [ myUsername, setMyUsername ] = useState ('')
-    const [ myActivities, setMyActivities ] = useState('')
-
-
-    useEffect(() => {
-
-        console.log(localStorage.getItem("token"))
-        if (localStorage.getItem("token")) {
-            props.setIsLoggedIn(true)
-            console.log("this is working")
-        } else {
-            props.setIsLoggedIn(false);
-            console.log("no token exists")
-        }
-    }, []);
+    const [ myRoutines, setMyRoutines ] = useState([])
+    
 
     const myData = async () => {
 
@@ -30,25 +18,31 @@ const UserRoutines = (props) => {
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${localStorage.getItem("token")}`,
-            },
-            body: JSON.stringify({
-                currentUser: {
-
-                }
-
-            })
+            }
         })
         const result = await response.json()
         console.log(result);
-        return result
+        setMyRoutines(result);
+        
     } catch (error) {
         console.log(error)
+
     }
     }
-    
+
+    useEffect(() => {
+        // if cuurentUser is true 
+        myData();
+    }, [])
+    console.log(myRoutines)
+    // filter through data here 
+    // filteredRoutines 
     return (
+        // render data 
         <p> Placeholder</p>
     )
 }
 
 export default UserRoutines
+
+
