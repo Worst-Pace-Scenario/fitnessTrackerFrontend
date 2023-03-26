e306b133a540b860c596096a635e1a1e21e9e323
Aamna Syed <aamna.s.syed@gmail.com>
2023-03-26 16:18:03 -0500

    charlies css

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -1,18 +1,20 @@
 import { useState } from "react"
 import { useParams, useNavigate,Link } from "react-router-dom"
+import "./Routines.css"
 
 const Routines = (props) => {
     let filteredRoutine = props.routines
 
     return (
         <div>
+            <h3 id="header"> Here you can find all the routines </h3>
             {
             filteredRoutine.length > 0 ? (filteredRoutine.map((singleRoutinesElement) => {
                    return (
                    <Link to= {`/routines/${singleRoutinesElement.id}`}>
-                        <div key={singleRoutinesElement.id}> 
+                        <div id="routines" key={singleRoutinesElement.id}> 
                             
-                            <h2>{singleRoutinesElement.name}</h2>
+                            <h2 id="link">{singleRoutinesElement.name}</h2>
                             <h4>{singleRoutinesElement.goal}</h4>
                             
                         </div>
